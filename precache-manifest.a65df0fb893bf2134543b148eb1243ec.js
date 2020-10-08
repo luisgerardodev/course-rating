5cb7370fb52fbb128be90b080c9030db15a0c868
Malui <yaminolight@gmail.com>
2020-10-07 21:44:01 -0400

    Updates

---
+++ precache-manifest.a65df0fb893bf2134543b148eb1243ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b607b924adbfc53a901b631a4475d539",
+    "url": "/luisgerardodev/course-rating.git/index.html"
+  },
+  {
+    "revision": "8e9fa91d687c9fcd9999",
+    "url": "/luisgerardodev/course-rating.git/static/css/main.cd853d54.chunk.css"
+  },
+  {
+    "revision": "79acf20cb576cf100c10",
+    "url": "/luisgerardodev/course-rating.git/static/js/2.16936fff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/luisgerardodev/course-rating.git/static/js/2.16936fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9fa91d687c9fcd9999",
+    "url": "/luisgerardodev/course-rating.git/static/js/main.efdd9ace.chunk.js"
+  },
+  {
+    "revision": "143dcfd75eb01e1335a5",
+    "url": "/luisgerardodev/course-rating.git/static/js/runtime-main.c73201a8.js"
+  }
+]);
